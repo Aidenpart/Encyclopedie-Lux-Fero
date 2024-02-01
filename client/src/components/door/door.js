f621c299a8806bc72aecf9ac685bfea6b58d1913
PC_Suli <sulivan.geffroy@hotmail.fr>
2024-02-01 13:31:52 +0100

    optimisation code component Door

--- client/src/components/door/door.js
+++ client/src/components/door/door.js
@@ -2,9 +2,8 @@ import "./styles.scss";
 
 export const Door = () => {
 
-
     return (
-        <div className="wrapper">
+        <div className="lintel">
             <img src="/images/link_Regina-Magicae.jpg" alt="link_RM"/>
             <div id="left-door" className="door">
                 <div className="shape"></div>
