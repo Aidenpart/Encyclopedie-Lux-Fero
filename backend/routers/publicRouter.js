f3d10f9b1f00f01bef76b8d984e40019374451ec
PC_Suli <sulivan.geffroy@hotmail.fr>
2024-02-29 16:36:18 +0100

    ajout route fiche dans public router back

--- backend/routers/publicRouter.js
+++ backend/routers/publicRouter.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {getLieu, readLieux} from '../controllers/lieuxControllers.js';
 import {getPersonnage, readPersonnages} from '../controllers/personnagesControllers.js';
 import {getRoman, readRomans} from '../controllers/romanControllers.js';
+import { getFiche, readFiches } from '../controllers/ficheController.js';
 
 
 
@@ -15,6 +16,9 @@ router.get("/wiki/personnages/get-personnage/:id", getPersonnage);
 
 router.get("/wiki/romans", readRomans);
 router.get("/wiki/romans/get-roman/:id", getRoman);
+
+router.get("/wiki/fiches", readFiches);
+router.get("/wiki/fiches/get-fiche/:id", getFiche);
 
 
 export default router;
